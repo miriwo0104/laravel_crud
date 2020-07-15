8c9947f03e6d8709374bdadfa95e3e1262a501ef
大川峻 <okawashun@MacBook-Pro.local>
2020-07-15 14:01:39 +0900

    Laravel CRUD処理を使った投稿アプリを作成する その6
    その6までのコードを登録する

---
+++ app/Http/Controllers/ContentController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Content;
+
+class ContentController extends Controller
+{
+    public function input()
+    {
+        return view('contents.input');
+    }
+
+    public function save(Request $request)
+    {
+        $input_content = new Content();
+        $input_content->content = $request['content'];
+        $input_content->save();
+        
+        return redirect('/output');
+    }
+
+    public function output()
+    {
+        $contents_get_query = Content::select('*');
+        $all_contents = $contents_get_query->get();
+        
+        return view('contents.output', [
+            'all_contents' => $all_contents,
+        ]);
+    }
+
+    public function delete(Request $request)
+    {
+        $contents_delete_query = Content::select('*');
+        $contents_delete_query->where('id', $request['content_id']);
+        $contents_delete_query->delete();
+
+        return redirect('/output');
+    }
+
+    public function edit($content_id)
+    {
+        $contents_edit_query = Content::select('*');
+        $contents_edit_query->where('id', $content_id);
+        $edit_contents = $contents_edit_query->get();
+        $edit_content = $edit_contents[0];
+
+        return view('contents.edit', [
+            'edit_content' => $edit_content,
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $contents_update_query = Content::select('*');
+        $contents_update_query->where('id', $request['content_id']);
+        $update_contents = $contents_update_query->get();
+
+        $update_content = $update_contents[0];
+        $update_content->content = $request['content'];
+        $update_content->save();
+
+        return redirect('/output');
+    }
+}
